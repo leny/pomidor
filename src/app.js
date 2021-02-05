849f8746173a52701627181e98ae5a3d5a2afec9
leny <info@flatland.be>
2021-02-06 00:45:40 +0100

    Simple React scaffolding

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/* leny/pomidor
+ *
+ * /src/app.js - Main entry point
+ *
+ * coded by leny@BeCode
+ * started at 06/02/2021
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from "./components/hello";
+
+ReactDOM.render(<Hello />, document.querySelector("#app"));
---
+++ src/components/hello.js
@@ -0,0 +1,13 @@
+/* leny/pomidor
+ *
+ * /src/components/hello.js - Hello Component
+ *
+ * coded by leny@BeCode
+ * started at 06/02/2021
+ */
+
+import React from "react";
+
+const Hello = () => <p>{"Hello, World!"}</p>;
+
+export default Hello
