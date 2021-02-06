92f56c8ae4d8b5f97fd0d2173f106def9ac4ccf5
leny <info@flatland.be>
2021-02-06 15:25:01 +0100

    Make the colon blink in Display

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -12,7 +12,7 @@ import Cyphers from "./cyphers";
 const Display = ({seconds}) => (
     <div className={"timer-display"}>
         <Cyphers value={Math.floor(seconds / 60)} />
-        <span>{":"}</span>
+        <span style={{opacity: seconds % 2 ? 0.5 : 1}}>{":"}</span>
         <Cyphers value={seconds % 60} />
     </div>
 );
