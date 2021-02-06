6b761fb3f0fcbbefd4eb06453d06db6492f307db
leny <info@flatland.be>
2021-02-06 01:06:38 +0100

    Setup react automatic runtime

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
+/* leny/pomidor
+ *
+ * /.eslintrc.js - ESLint config
+ *
+ * coded by leny@BeCode
+ * started at 06/02/2021
+ */
+
 module.exports = {
     extends: "@becode",
+    rules: {"react/jsx-uses-react": 0, "react/react-in-jsx-scope": 0},
 };
