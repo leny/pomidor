f7e1db1e4376deab051fa06f96fe47a51dcb0fa4
leny <info@flatland.be>
2021-02-06 01:11:06 +0100

    Use PropTypes & props in Hello

--- src/components/hello.js
+++ src/components/hello.js
@@ -6,6 +6,12 @@
  * started at 06/02/2021
  */
 
+import PropTypes from "prop-types";
+
 const Hello = ({name}) => <p>{`Hello, ${name}!`}</p>;
+
+Hello.propTypes = {
+    name: PropTypes.string.isRequired,
+};
 
 export default Hello;
